695483cfd5da5cfc94fcad03bd3a96be3c14a1ad
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-08-27 01:30:03 -0700

    Adds set for forecast data

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,6 +60,8 @@ const currentDay = (() => {
         const date = dayjs().format('MM/DD/YYYY');
         let icon;
         const temp = Math.round(currentDayData.main.temp);
+        const high = Math.round(currentDayData.main.temp_max);
+        const low = Math.round(currentDayData.main.temp_min);
         const wind = Math.round(currentDayData.wind.speed);
         const humidity = Math.round(currentDayData.main.humidity);
         const time = dayjs.unix(currentDayData.dt).format('HH:mm:ss');
@@ -77,9 +79,9 @@ const currentDay = (() => {
         const currentDayCard = document.getElementById('currentDay');
         currentDayCard.insertAdjacentHTML('afterbegin', 
            `<h1 class="fw-bolder pb-3">${currentDayCity}</h1>
-            <p>(${date})</p>
+            <p>${date}</p>
             <p>${icon}</p>
-            <p>Temp: ${temp}°F</p>
+            <p>Temp: ${temp}°F / High: ${high}°F / Low: ${low}°F</p>
             <p>Wind: ${wind} MPH</p>
             <p>Humidity: ${humidity}%</p>`
         );
@@ -88,24 +90,31 @@ const currentDay = (() => {
 
 currentDay();
 
-// set up 5 day forecasting
+const forecastData = (() => {
+    const forecastApiCall = `${apiUrl}forecast?q=${JSON.parse(localStorage.getItem('selectedCity'))},us&units=imperial&appid=${apiKey}`
+    fetch(forecastApiCall)
+    .then(response => {
+            response.json()
+            .then((forecastData) => {
+                let forecastArr = forecastData.list;
+                forecastArr.forEach(data => {data.dt_txt = data.dt_txt.slice(0, 10)});              
+                const dateArr = forecastArr.map(( data ) => data.dt_txt);
+                const filterArr = forecastArr.filter((data, i) =>
+                !dateArr.includes(data.dt_txt, i + 1));
+                filterArr.shift();
+                forecastArr = filterArr;               
+                localStorage.setItem('forecastData', JSON.stringify(forecastArr));
+            })
+            .catch(error => console.error('Error Fetching Forecast Data:', error));  
+        })
+    .catch(error => console.error('Error Failed Connecting:', error));
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+forecastData();
 
 
 // testing ----------------------------------------------------------------------------------------------------
+
+
 
 // end testing ------------------------------------------------------------------------------------------------
